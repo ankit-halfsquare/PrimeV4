597f88383a517704e10b4ea03b4c74e9b7f392de
abhishek@halfsquare.in <abhishek@halfsquare.in>
2023-01-05 18:23:12 +0530

    Add table

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name    
+
+
+
+class BuiltdingItem(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class BuildingSelectionList(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class BuildingSelectionListItem(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
